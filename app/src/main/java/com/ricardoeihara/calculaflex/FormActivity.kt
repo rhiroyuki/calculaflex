53cf18d306f3bd4eafa754fd124bb5610f6183a8
Ricardo Eihara <ricardo.hejr@gmail.com>
2019-04-27 10:21:27 -0300

    Add first part

---
+++ app/src/main/java/com/ricardoeihara/calculaflex/FormActivity.kt
@@ -0,0 +1,30 @@
+package com.ricardoeihara.calculaflex
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ricardoeihara.calculaflex.watchers.DecimalTextWatcher
+import kotlinx.android.synthetic.main.activity_form.*
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+
+        etGasPrice.addTextChangedListener(DecimalTextWatcher(etGasPrice))
+        etEthanolPrice.addTextChangedListener(DecimalTextWatcher(etEthanolPrice))
+        etGasAverage.addTextChangedListener(DecimalTextWatcher(etGasAverage, 1))
+        etEthanolAverage.addTextChangedListener(DecimalTextWatcher(etEthanolAverage, 1))
+
+        btCalculate.setOnClickListener {
+            val proximatela = Intent(this@FormActivity, ResultActivity::class.java)
+            proximatela.putExtra("GAS_PRICE", etGasPrice.text.toString().toDouble())
+            proximatela.putExtra("ETHANOL_PRICE", etEthanolPrice.text.toString().toDouble())
+            proximatela.putExtra("GAS_AVERAGE", etGasAverage.text.toString().toDouble())
+            proximatela.putExtra("ETHANOL_AVERAGE", etEthanolAverage.text.toString().toDouble())
+            startActivity(proximatela)
+        }
+
+    }
+}
